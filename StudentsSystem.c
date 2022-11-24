909bed5ca4ae723027f14ca809b5da9a118ff24e
Omer Kriger <83721785+OmerKriger@users.noreply.github.com>
2022-11-24 17:18:53 +0200

    Update StudentsSystem.c

--- StudentsSystem.c
+++ StudentsSystem.c
@@ -2,17 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-/*
-Pogram summary:
-The pogram is system that get information about 3 group of students that 6 student in each group
-the pogram get Full name, ID, courses and grades.
-ask the user for specific course number and give him the students in this group and change specific name (Lois) to different name (Lane)
-and remove from the list students with the name "Lex"
-and sum up the best grade and second of the course from each group
-*/
-
 // Defines
-
 #define GROUP_A 0
 #define GROUP_B 1
 #define GROUP_C 2
